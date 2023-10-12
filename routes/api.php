a026aaedd5c9a86a50a3c01179e3dee77fc64b35
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2023-10-12 19:35:23 +0300

    added google authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GoogleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'google/login'], function () {
+    Route::get('url', [GoogleController::class, 'getAuthUrl']);
+    Route::post('', [GoogleController::class, 'postLogin']);
+});
+
+Route::middleware(['auth:api'])->group(function () {
+    // Here will be authorized routes
 });
